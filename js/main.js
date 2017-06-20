1d817a898e22f16ffd51fe70d70dd9c1c90b9f40
Tolu Akinnubi <tolu.akinnubi@yahoo.com>
2017-06-20 23:43:12 +0100

    add input to list

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 // To do list
+function addToDo(){
+
+	var inputValue = document.getElementById("inputValue").value;
+	var text = document.createTextNode(inputValue);
+	var ul =  document.getElementById("addedItems");
+	var li  = document.createElement("li");
+
+	// To add the todo text to an <li>
+	li.appendChild(text);
+
+
+	if(inputValue === ""){
+
+		alert("Empty Textfield");
+	}else{
+
+		ul.appendChild(li);
+	}
+
+	document.getElementById("inputValue").value = " ";
+
+}
